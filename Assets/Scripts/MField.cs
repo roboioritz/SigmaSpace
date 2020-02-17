a43c861b80bd82b053a2ab4c1e3c93f2a4bfde69
roboioritz <48485367+roboioritz@users.noreply.github.com>
2020-02-17 08:00:10 +0100

    cargado

---
+++ Assets/Scripts/MField.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MField : MonoBehaviour
+{
+    public Transform parent;
+    public float radio;
+    public GameObject rallos;
+    public GameObject r;
+    public bool charged;
+
+
+    private void Start()
+    {
+        //GetComponent<SphereCollider>().radius = radio;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Asteroid>().type == "Charged"&& !charged)
+        {
+            charged = true;
+            Vector3 vector = ((parent.position - other.transform.position) / 2) + other.transform.position;
+            //float dist = Mathf.Sqrt((vector.x * vector.x) + (vector.y * vector.y));
+            //Instantiate(rallos, vector, Quaternion.Euler(0, Mathf.Rad2Deg*(Mathf.Asin(vector.y/dist)), 0));
+            r = Instantiate(rallos, vector, Quaternion.identity);
+            r.GetComponent<Thunder>().T1 = parent.gameObject;
+            r.GetComponent<Thunder>().T2 = other.gameObject;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<Asteroid>().type == "Charged" && !charged)
+        {
+            charged = true;
+            Vector3 vector = ((parent.position - other.transform.position) / 2) + other.transform.position;
+            //float dist = Mathf.Sqrt((vector.x * vector.x) + (vector.y * vector.y));
+            //Instantiate(rallos, vector, Quaternion.Euler(0, Mathf.Rad2Deg*(Mathf.Asin(vector.y/dist)), 0));
+            r = Instantiate(rallos, vector, Quaternion.identity);
+            r.GetComponent<Thunder>().T1 = parent.gameObject;
+            r.GetComponent<Thunder>().T2 = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(r);
+        charged = false;
+    }
+}
---
+++ Assets/Scripts/Thunder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thunder : MonoBehaviour
+{
+    public GameObject T1;
+    public GameObject T2;
+
+    private float dist;
+
+    void Start()
+    {
+        //transform.rotation = Quaternion.Euler(90,0,0);
+    }
+
+    
+    void Update()
+    {
+        transform.LookAt(T1.transform);
+
+        transform.position = ((T1.transform.position - T2.transform.position) / 2) + T2.transform.position;
+        dist = Mathf.Sqrt(Mathf.Pow(T1.transform.position.x- T2.transform.position.x,2)+ Mathf.Pow(T1.transform.position.y - T2.transform.position.y, 2)+ Mathf.Pow(T1.transform.position.z - T2.transform.position.z, 2));
+        transform.localScale = new Vector3(1,1,dist*0.5f);
+    }
+}
